e2ee43901e1897ff072211b54bf843990d270ced
seigo2016 <2003seigo@gmail.com>
2022-10-02 18:53:56 +0900

    2.5 return文の構文解析

--- 01/src/monkey/ast/ast.go
+++ 01/src/monkey/ast/ast.go
@@ -32,8 +32,15 @@ type LetStatement struct {
 	Value Expression
 }
 
-func (ls *LetStatement) statementNode()       {}
-func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (ls *LetStatement) statementNode()          {}
+func (ls *LetStatement) TokenLiteral() string    { return ls.Token.Literal }
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
 type Identifier struct {
 	Token token.Token
